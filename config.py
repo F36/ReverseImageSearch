3c75be0e98ce32bdf49c990b1f23122c50749bfd
F36 <70635838+F36@users.noreply.github.com>
2020-09-25 14:38:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config(object):
-    CLIENT_ID = ""
-    BOT_TOKEN = ""
-    HEROKU_APP_NAME = ""
+    CLIENT_ID = "5bd3ce843b5cfdf"
+    BOT_TOKEN = "1135136964:AAHEA0h7QIWV3juJMAz2JvzPmXLcnhMfhTw"
+    HEROKU_APP_NAME = "imgurtas"
